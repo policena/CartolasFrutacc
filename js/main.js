111f15eb497621c9507a3c3d83310bf47e0493c0
Tiago Policena Martins <tiagopolicena@frutacc.com.br>
2017-06-19 07:58:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,34 +28,32 @@ console.log('ok');
         });
 
     }
-
-    function montaTime(data) {
+  function montaTime(data) {
         var imgEscudo = data.time.url_escudo_png;
         var imgPerfil = data.time.foto_perfil;
 
         var nome = data.time.nome;
         var nome_cartola = data.time.nome_cartola;
-        var pontos = data.pontos ? data.pontos.toFixed(2) : 0;
+        var pontos = data.pontos.toFixed(2);
         var patrimonio = data.patrimonio;
 
-        var atletas_html = '';
-        if (typeof atletas_pontuados !== 'undefined')
-            atletas_html = createAtletasTimeHtml(data.atletas);
-
-        var parcial_rodada = (typeof atletas_pontuados !== 'undefined') ? total_pontos.toFixed(2) : pontos;
+        var atletas_html = createAtletasTimeHtml(data.atletas, data.posicoes);
+        var parcial_rodada = total_pontos.toFixed(2);
         var slug_time = data.time.slug;
+        var pontos_ordenacao = (total_pontos == 0.00 && !mercadoFechado) ? pontos : total_pontos;
+        var pro = data.time.assinante == true ? '<img src="https://cartolafc.globo.com/dist/0.6.9/img/selo-cartoleiro-pro.svg" class="cartola-pro">' : '';
 
-        $('#narnia-table').append('<tr class="' + slug_time + '" data-row="' + slug_time + '" data-total="' + parcial_rodada + '"><td colspan="1"><div class="col-xs-12"><img src="' + imgEscudo + '" style="width: 50px;"><img style="width: 30px;position: absolute;left: 45px;top: 25px;" src="' + imgPerfil + '" class="img-circle"></div></td><td colspan="3"><h3>' + nome + '</h3><p>' + nome_cartola + '</p></td><td colspan="2"><p class="ponto" style="text-align: center">' + pontos + '</p></td><td colspan="2" style="text-align: center"><p class="pontoparcial">' + parcial_rodada + '</p></td><td colspan="2" style="text-align: center;"><p class="patrimonio">' + patrimonio + '</p></td><td colspan="2" style="text-align: center" class="coca"></td></tr>');
+        $('#narnia-table').append('<tr class="' + slug_time + '" data-row="' + slug_time + '" data-total="' + pontos_ordenacao + '"><td colspan="1"><div class="col-xs-12">' + pro + '<img src="' + imgEscudo + '" style="width: 50px;"><img style="width: 30px;position: absolute;left: 45px;top: 25px;" src="' + imgPerfil + '" class="img-circle"></div></td><td colspan="3"><h3>' + nome + '</h3><p>' + nome_cartola + '</p></td><td colspan="2"><p class="ponto" style="text-align: center">' + pontos + '</p></td><td colspan="2" style="text-align: center"><p class="pontoparcial">' + parcial_rodada + '</p></td><td colspan="2" style="text-align: center;"><p class="patrimonio">' + patrimonio + '</p></td><td colspan="2" style="text-align: center" class="coca"></td></tr>');
         if (atletas_html != '')
             $('#narnia-table').append('<tr class="' + slug_time + '"	>' + atletas_html + '</tr>');
 
         total_pontos = 0.00;
     }
 
-    function createAtletasTimeHtml(atletas_time) {
+    function createAtletasTimeHtml(atletas_time, posicoes) {
         var atletas = '';
         for (var i = 0; i < atletas_time.length; i++) {
-            atletas += getTemplateAtleta(atletas_time[i]);
+            atletas += getTemplateAtleta(atletas_time[i], posicoes);
         }
         return atletas;
     }
@@ -69,82 +67,90 @@ console.log('ok');
             total_pontos += pontuacao;
         }
 
-        var foto = data.foto;
-        if (foto == null) foto = '';
+        Var foto =  dados . Foto ;
+        Se (foto ==  null ) foto =  ' ' ;
+        Var posicao = posicoes [ dados . Posicao_id ]. Abreviacao ;
 
-        return '<td><div class="col-xs-12">' +
-            '<p style="font-size: small">' + data.apelido + '</p>' +
-            '<img style="width: 40px;" src="' + foto.replace("FORMATO", "140x140") + '">' +
-            '<p>' + pontuacao + '<p>' +
-            '</div></td>';
+        Retornar  ' <td> <div class = "col-xs-12"> '  +
+            ' <P style = "font-size: small"> '  +  data . Apelido  +  ' ( '  + posicao +  ' ) '  +  ' </ p> '  +
+            ' <Img style = "width: 40px;" src = " '  +  foto . substituir ( " FORMATO " , " 140x140 " ) +  ' "> '  +
+            ' <P> '  + pontuacao +  ' <p> '  +
+            ' </ Div> </ td> ' ;
     }
 
-    function quem_paga() {
-        return null;
-        var menorObj = $('.pontoparcial').first();
-        var menorValor = parseFloat($('.pontoparcial').first().text());
-        $('.pontoparcial').each(function (i, obj) {
-            if (parseFloat($(obj).text()) < menorValor) {
-                menorObj = obj;
-                menorValor = parseFloat($(obj).text());
+    Função  quem_paga () {
+        Var menorObj =  $ (classeOrdenação). Primeiro ();
+        Var menorValor =  parseFloat ( $ (classeOrdenação). Primeiro (). Text ());
+        $ (ClasseOrdenação). Cada ( função ( i , obj ) {
+            Se ( parseFloat ( $ (obj). Text ()) < menorValor) {
+                MenorObj = obj;
+                MenorValor =  parseFloat ( $ (obj). Text ());
             }
-        });
+        })
 
-        var parent = $(menorObj).parent().parent();
-        parent.addClass('paga-coca');
-        $.get('https://cors-anywhere.herokuapp.com/https://api.riffsy.com/v1/search?tag=coca-cola&key=LIVDSRZULELA', function (data) {
-            parent.find('.coca').append('<img src="' + data.results[Math.floor(Math.random() * 19)].media[0].gif.url + '" style="height: 100px;">')
-        });
+        Var parent =  $ (menorObj). Pai (). Pai ();
+        Pai . AddClass ( ' paga-coca ' );
+        $ . Get ( ' http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=coca-cola ' , função ( dados ) {
+            Pai . encontrar ( ' .coca ' ). Anexar ( ' <img src = " '  +  dados . Dados . Image_url  +  ' " style = "height: 100px;"> ' )
+        })
     }
 
-    function ordena() {
-        var rows = $('#narnia-table > tbody > tr');
-        var data_row = [], data_total = [], data_row_attr = '', data_total_attr = '';
-        for (var i = 0; i < rows.length; i++) {
-            data_row_attr = $(rows[i]).data('row');
-            data_total_attr = $(rows[i]).data('total');
+    Função  mito () {
+        Var theLegend =  $ ( ' # narnia-table> tbody> tr ' ) [ 0 ];
+        $ (TheLegend). AddClass ( ' mito ' );
+        $ . Get ( ' http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=win ' , função ( dados ) {
+            $ (TheLegend). encontrar ( ' .coca ' ). Anexar ( ' <img src = " '  +  dados . Dados . Image_url  +  ' " style = "height: 100px;"> ' )
+        })
+    }
 
-            if (typeof data_row_attr !== 'undefined') data_row.push(data_row_attr);
-            if (typeof data_total_attr !== 'undefined') data_total.push(data_total_attr);
-        }
-        var arrr = [];
-        for (var h = 0; h < data_row.length; h++) {
-            arrr[h + '-' + data_total[h]] = data_row[h];
-        }
+    Função  ordena () {
+        Var rows =  $ ( ' # narnia-table> tbody> tr ' );
+        Var data_row = [], data_total = [], data_row_attr =  ' ' , data_total_attr =  ' ' ;
+        para ( var i =  0 ; i <  fileiras . comprimento ; i ++ ) {
+            Data_row_attr =  $ (linhas [i]). Dados ( ' linha ' );
+            Data_total_attr =  $ (linhas [i]). Dados ( ' total ' );
 
-        var ordenado = [];
-        data_total = data_total.sort(function (a, b) {
-            return b - a;
-        });
-        for (var t = 0; t < data_total.length; t++) {
-            for (var x = 0; x < data_total.length; x++) {
-                var arrr2 = arrr[x + '-' + data_total[t]];
-                if ((typeof arrr2 !== 'undefined') && (ordenado.indexOf(arrr2) == -1))
-                    ordenado.push(arrr2);
-            }
+            Se ( typeof data_row_attr ! ==  ' indefinido ' ) data_row . empurrão (data_row_attr);
+            Se ( typeof data_total_attr ! ==  ' indefinido ' ) data_total . Push (data_total_attr);
+        }
+        Var arrr = [];
+        Para ( var h =  0 ; h <  data_row . Length ; h ++ ) {
+            Arrr [h +  ' - '  + data_total [h]] = data_row [h];
         }
 
-        var row_o = '';
-        $(ordenado).each(function (i, obj) {
-            row_o = $("#narnia-table > tbody > ." + obj);
-            $('#narnia-table > tbody').append(row_o);
-        });
-    }
-
-    cartola.initialize = function () {
-        $(document).ready(function () {
-            get_pontuacao_atletas();
-        }).ajaxStop(function () {
-            if (qtdeTimesProcessados == times.length && !finalizouProcessamento) {
-                finalizouProcessamento = true;
-                quem_paga();
-                ordena();
-
-                $('#narnia-table').show();
-                $('#spinner').hide();
+        Var ordenado = [];
+        Data_total =  data_total . Classificar ( função ( a , b ) {
+            Retornar b - a;
+        })
+        Para ( var t =  0 ; t <  data_total . Length ; t ++ ) {
+            Para ( var x =  0 ; x <  data_total . Length ; x ++ ) {
+                Var arrr2 = arrr [x +  ' - '  + data_total [t]];
+                Se (( typeof arrr2 ! ==  ' indefinido ' ) && ( ordenado . IndexOf (arrr2) ==  - 1 ))
+                    Ordenado . Empurrar (arrr2);
             }
-        });
-    }
-}(Narnia));
+        }
 
-Narnia.initialize();
+        Var row_o =  ' ' ;
+        $ (Ordenado). Cada ( função ( i , obj ) {
+            Row_o =  $ ( " # narnia-table> tbody>. "  + Obj);
+            $ ( ' # Narnia-table> tbody ' ). Anexar (row_o);
+        })
+    }
+
+    Cartola . Inicializar  =  função () {
+        $ ( Documento ). Pronto ( function () {
+            Get_status_mercado ();
+            Get_pontuacao_atletas ();
+        }). AjaxStop ( function () {
+            Se ( ! Processado &&  times . Length  == timesProcessados) {
+                processado =  verdadeiro ;
+                Quem_paga ();
+                Ordena ();
+                Mito ();
+                $ ( ' # Narnia-table ' ). Show ();
+                $ ( ' #spinner ' ). Hide ();
+            }
+        })
+    }
+}(Nárnia));
+
